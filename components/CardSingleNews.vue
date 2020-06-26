c144cc16489f55ac8447f52008c8c1dd0b0bca92
OziOcb <pawel_ozi@o2.pl>
2020-06-26 15:14:15 +0100

    Create a base CardSingleNews.vue component

---
+++ components/CardSingleNews.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <a :href="article.url" target="_blank" rel="noopener noreferrer">
+      <img :src="article.urlToImage" :alt="article.title" />
+      <div>
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+      </div>
+      <footer>
+        {{ article.publishedAt }}
+      </footer>
+    </a>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
