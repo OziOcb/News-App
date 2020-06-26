5f0a3d0b4bc320d66c8cdc7bf5d8a73b624901f6
OziOcb <pawel_ozi@o2.pl>
2020-06-26 15:39:52 +0100

    Create a grid for the news list

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <button @click="setArticlesHandler">hit me</button>
 
-    <ul>
+    <ul class="grid row-gap-12 sm:grid-cols-2 xl:grid-cols-3 sm:col-gap-6">
       <li v-for="(article, index) in getArticles" :key="index">
         <CardSingleNews :article="article" />
       </li>
